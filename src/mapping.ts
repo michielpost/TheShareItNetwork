6caa3204954b9c1f306496eaf697d879bce5b3a1
michielpost <michiel@q42.nl>
2021-05-25 13:15:14 +0200

    Added The Graph files

---
+++ src/mapping.ts
@@ -0,0 +1,16 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  TheShareItNetwork,
+  ShareIt
+} from "../generated/TheShareItNetwork/TheShareItNetwork"
+import { Share } from "../generated/schema"
+
+export function handleShareIt(event: ShareIt): void {
+  let share = new Share(event.params.id.toHex())
+  share.sender = event.params.sender
+  share.receiver = event.params.receiver
+  share.appId = event.params.appId
+  share.shareType = event.params.shareType
+  share.shareData = event.params.data
+  share.save()
+}
